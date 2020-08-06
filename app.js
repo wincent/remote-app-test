7e105a89915f22713ced5973608b73e96bee2644
Greg Hurrell <greg.hurrell@liferay.com>
2020-08-06 18:01:28 +0200

    refactor: show toast in response to a button click

--- app.js
+++ app.js
@@ -3,12 +3,20 @@
 
   console.log('app.js', window.__LIFERAY_REMOTE_WEB_APP_SDK__);
 
+  // TODO show that this still works if we load first
   try {
     __LIFERAY_REMOTE_WEB_APP_SDK__.postMessage({key: 'value'});
-    __LIFERAY_REMOTE_WEB_APP_SDK__.openToast({
-      message: 'Hello from the other side',
-    });
   } catch (error) {
     console.log(error);
   }
+
+  document.getElementById('hello').addEventListener('submit', function() {
+    try {
+      __LIFERAY_REMOTE_WEB_APP_SDK__.openToast({
+        message: 'Hello from the other side',
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  });
 })();
