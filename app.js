e7aad1e177a99abe420c341e751715df1b40590b
Greg Hurrell <greg.hurrell@liferay.com>
2020-08-11 12:58:01 +0200

    refactor: add a helper function to reduce repetition

--- app.js
+++ app.js
@@ -9,56 +9,50 @@
     return;
   }
 
-
-  try {
-    SDK.openToast({
-      message: 'Welcome to the danger zone',
-      type: 'danger',
+  function button(id, handler) {
+    document.getElementById(id).addEventListener('click', function() {
+      try {
+        handler(event);
+      } catch(error) {
+        console.error(error);
+      }
     });
-  } catch (error) {
-    console.log(error);
   }
 
-  document.getElementById('hello').addEventListener('click', function() {
-    try {
-      SDK.openToast({
-        message: 'Hello from the other side',
-      });
-    } catch (error) {
-      console.log(error);
-    }
+
+  SDK.openToast({
+    message: 'Welcome to the danger zone',
+    type: 'danger',
   });
 
-  document.getElementById('extend').addEventListener('click', function() {
-    try {
-      SDK.fetch(
-        'http://0.0.0.0:8080/c/portal/extend_session'
-      ).then(function () {
-        console.log('extended');
-      }).catch(function(error) {
-        console.log('caught', error);
-      });
-    } catch (error) {
-      console.log(error);
-    }
+  button('hello', function() {
+    SDK.openToast({
+      message: 'Hello from the other side',
+    });
   });
 
-  document.getElementById('get').addEventListener('click', function() {
+  button('extend', function() {
+    SDK.fetch(
+      'http://0.0.0.0:8080/c/portal/extend_session'
+    ).then(function () {
+      console.log('extended');
+    }).catch(function(error) {
+      console.log('caught', error);
+    });
+  });
+
+  button('get', function() {
     // NOTE: Using "guest" as siteID is naughty, but I don't want to rely on the
     // potentially unstable numeric ID from my dev environment (20122).
-    try {
-      SDK.fetch(
-        'http://0.0.0.0:8080/o/headless-delivery/v1.0/sites/guest/structured-contents/'
-      ).then(function (response) {
-        console.log('got response');
-        return response.json();
-      }).then(function (json) {
-        console.log('got JSON', json);
-      }).catch(function(error) {
-        console.log('caught', error);
-      });
-    } catch (error) {
-      console.log(error);
-    }
+    SDK.fetch(
+      'http://0.0.0.0:8080/o/headless-delivery/v1.0/sites/guest/structured-contents/'
+    ).then(function (response) {
+      console.log('got response');
+      return response.json();
+    }).then(function (json) {
+      console.log('got JSON', json);
+    }).catch(function(error) {
+      console.log('caught', error);
+    });
   });
 })();
