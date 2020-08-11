79c1927ffb916e80b09b24634608f40c34ad5db2
Greg Hurrell <greg.hurrell@liferay.com>
2020-08-11 13:04:42 +0200

    style: format with Prettier

--- app.js
+++ app.js
@@ -1,58 +1,61 @@
-(function() {
-  'use strict';
+(function () {
+	'use strict';
 
-  var SDK = window.__LIFERAY_REMOTE_WEB_APP_SDK__;
+	var SDK = window.__LIFERAY_REMOTE_WEB_APP_SDK__;
 
-  if (!SDK) {
-    console.error('No __LIFERAY_REMOTE_WEB_APP_SDK__ on window');
+	if (!SDK) {
+		console.error('No __LIFERAY_REMOTE_WEB_APP_SDK__ on window');
 
-    return;
-  }
+		return;
+	}
 
-  function button(id, handler) {
-    document.getElementById(id).addEventListener('click', function() {
-      try {
-        handler(event);
-      } catch(error) {
-        console.error(error);
-      }
-    });
-  }
+	function button(id, handler) {
+		document.getElementById(id).addEventListener('click', function () {
+			try {
+				handler(event);
+			} catch (error) {
+				console.error(error);
+			}
+		});
+	}
 
+	SDK.openToast({
+		message: 'Welcome to the danger zone',
+		type: 'danger',
+	});
 
-  SDK.openToast({
-    message: 'Welcome to the danger zone',
-    type: 'danger',
-  });
+	button('hello', function () {
+		SDK.openToast({
+			message: 'Hello from the other side',
+		});
+	});
 
-  button('hello', function() {
-    SDK.openToast({
-      message: 'Hello from the other side',
-    });
-  });
+	button('extend', function () {
+		SDK.fetch('http://0.0.0.0:8080/c/portal/extend_session')
+			.then(function () {
+				console.log('extended');
+			})
+			.catch(function (error) {
+				console.log('caught', error);
+			});
+	});
 
-  button('extend', function() {
-    SDK.fetch(
-      'http://0.0.0.0:8080/c/portal/extend_session'
-    ).then(function () {
-      console.log('extended');
-    }).catch(function(error) {
-      console.log('caught', error);
-    });
-  });
+	button('get', function () {
+		// NOTE: Using "guest" as siteID is naughty, but I don't want to rely on the
+		// potentially unstable numeric ID from my dev environment (20122).
+		SDK.fetch(
+			'http://0.0.0.0:8080/o/headless-delivery/v1.0/sites/guest/structured-contents/'
+		)
+			.then(function (response) {
+				console.log('got response');
 
-  button('get', function() {
-    // NOTE: Using "guest" as siteID is naughty, but I don't want to rely on the
-    // potentially unstable numeric ID from my dev environment (20122).
-    SDK.fetch(
-      'http://0.0.0.0:8080/o/headless-delivery/v1.0/sites/guest/structured-contents/'
-    ).then(function (response) {
-      console.log('got response');
-      return response.json();
-    }).then(function (json) {
-      console.log('got JSON', json);
-    }).catch(function(error) {
-      console.log('caught', error);
-    });
-  });
+				return response.json();
+			})
+			.then(function (json) {
+				console.log('got JSON', json);
+			})
+			.catch(function (error) {
+				console.log('caught', error);
+			});
+	});
 })();
